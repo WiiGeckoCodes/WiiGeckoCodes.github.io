7d442485651cb9a17a1b46473ca915b030711e26
BannerBomb <BannerBomb55@gmail.com>
2016-02-29 10:41:18 -0500

    Update template.js

--- template.js
+++ template.js
@@ -63,7 +63,6 @@ var template = '\
  	<a href="http://wiigeckocodes.github.io/buttonconditionals.html">Button Conditionals</a><br><br> \
  	<a href="http://wiigeckocodes.github.io/dataconversions.html">Data Conversions</a><br><br> \
  	<a href="http://wiigeckocodes.github.io/gctcreator.html">Online GCT Creator</a><br><br> \
- 	<a href="http://geckocodes.org/AH.php">Accio Hacks</a><br><br> \
  	<a href="http://wiigeckocodes.github.io/channels.html">Channel Select</a><br><br> \
  	<a href="http://github.com/BannerBomb/" target="_blank">Github</a><br><br> \
 </nav> \
